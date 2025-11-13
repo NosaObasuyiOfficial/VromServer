7d136054d530dc8c0426d9a2e4c83076563a852d
Nosa_Clement_Obasuyi <131542687+NosaObasuyiOfficial@users.noreply.github.com>
2025-11-13 01:35:37 +0100

    twilIO messages

--- src/controllers/vromFunctions.ts
+++ src/controllers/vromFunctions.ts
@@ -1,15 +1,22 @@
 import type { Request, Response } from "express";
 import { sendMessage } from "../twilioSetup/sendMessage.js";
 
-export const userRequest = async(req: Request, res: Response) => {
-    try{
-      const f = await sendMessage("+2348108505829", "You're already registered as a rider.");
-            console.log(f)
-    }catch(error){
-            res.status(500).json({
-                "message": "Cannot work"
-            })
-            console.error(error)
-    }
+export const userRequest = async (req: Request, res: Response) => {
+  try {
+    //   const f = await sendMessage("+2348108505829", "You're already registered as a rider.");
+    const f = await sendMessage(
+      "+2348108505829",
+      "👋 Welcome to RideBot!\n\nPlease choose an option:\n" +
+        "1️⃣ Register as Rider\n" +
+        "2️⃣ Request a Ride\n" +
+        "3️⃣ Help"
+    );
 
-}
+    console.log("function", f);
+  } catch (error) {
+    res.status(500).json({
+      message: "Cannot work",
+    });
+    console.error(error);
+  }
+};
